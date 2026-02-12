7f816c00cf8fca3d83b9fe23ba28e59d3a9019c8
Advait1804 <pradhanadvait18@gmail.com>
2026-02-12 19:02:37 +0530

    Refactor team identification in build_attack_json

--- Python/attack_json.py
+++ Python/attack_json.py
@@ -1,5 +1,4 @@
 import pandas as pd
-
 def build_attack_json(dfs):
     matches=dfs.get("match_details")
     teams=dfs.get("team")
@@ -9,21 +8,17 @@ def build_attack_json(dfs):
     if matches is None:
         return attack_list
     for _,m in matches.iterrows():
-        #print("in match")
         m_id=m.get("match_id")
         team_cols = []
         phase_dic={}
-        if "team_a" in m and pd.notna(m["team_a"]):
-            team_cols.append(m["team_a"])
-        if "team_b" in m and pd.notna(m["team_b"]):
-            team_cols.append(m["team_b"])
+        if "home_team" in m and pd.notna(m["home_team"]):
+            team_cols.append(m["home_team"])
+        if "away_team" in m and pd.notna(m["away_team"]):
+            team_cols.append(m["away_team"])
         for tid in team_cols:
             for _,a in attack.iterrows():
                 t_id=a.get("team_id")
-                #print("match_id: ",m_id,"attack_match_id: ",a["match_id"])
-                #print("team_id: ",tid,"attack team_id: ",a["team_id"])
                 if int(a["match_id"]) == int(m_id) and int(a["team_id"]) == int(tid):
-                    #print("in if") 
                     team_name=team_name_map.get(t_id,-1)
                     innings=a.get("inning")
                     points=a.get("points")
@@ -32,7 +27,6 @@ def build_attack_json(dfs):
                         phase_dic[innings] = {}
                     if phase not in phase_dic[innings]:
                         phase_dic[innings][phase] = {}
-                    phase_dic[innings][phase][team_name] = points
-        #print(team_cols)            
+                    phase_dic[innings][phase][team_name] = points            
         attack_list.append({m_id:phase_dic})
     return attack_list
