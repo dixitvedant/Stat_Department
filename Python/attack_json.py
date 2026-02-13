02e333fab82035b37b547021b25c1363d4a09854
Advait1804 <pradhanadvait18@gmail.com>
2026-02-13 09:47:44 +0530

    Refactor build_attack_json to use a dictionary

--- Python/attack_json.py
+++ Python/attack_json.py
@@ -3,11 +3,12 @@ def build_attack_json(dfs):
     matches=dfs.get("match_details")
     teams=dfs.get("team")
     attack=dfs.get("team_attack")
-    attack_list=[]
+    attack_dic={}
     team_name_map = {row.team_id: row.team_name for _, row in teams.iterrows()} if teams is not None else {}
     if matches is None:
-        return attack_list
+        return attack_dic
     for _,m in matches.iterrows():
+       
         m_id=m.get("match_id")
         team_cols = []
         phase_dic={}
@@ -18,15 +19,19 @@ def build_attack_json(dfs):
         for tid in team_cols:
             for _,a in attack.iterrows():
                 t_id=a.get("team_id")
+                
                 if int(a["match_id"]) == int(m_id) and int(a["team_id"]) == int(tid):
+                     
                     team_name=team_name_map.get(t_id,-1)
-                    innings=a.get("inning")
+                    inning=a.get("inning")
+                    innings="inning"+str(inning)
                     points=a.get("points")
                     phase=a.get("phase")
                     if innings not in phase_dic:
                         phase_dic[innings] = {}
                     if phase not in phase_dic[innings]:
                         phase_dic[innings][phase] = {}
-                    phase_dic[innings][phase][team_name] = points            
-        attack_list.append({m_id:phase_dic})
-    return attack_list
+                    phase_dic[innings][phase][team_name] = points
+                  
+        attack_dic[m_id]=phase_dic
+    return attack_dic
