d6a7fa2c372825d28f3e4fcec65ee35b6632a6b4
Advait1804 <pradhanadvait18@gmail.com>
2026-02-14 08:00:30 +0530

    Enhance build_defence_json with filter capabilities
    Added filtering options for tournament, season, match, and team IDs to the build_defence_json function.

--- Python/defence_json.py
+++ Python/defence_json.py
@@ -1,15 +1,47 @@
-import pandas as pd
 
-def build_defence_json(dfs):
+def build_defence_json(dfs,filters=None):
 
     matches = dfs.get("match_details")
     teams = dfs.get("team")
+    seasons=dfs.get("season")
     defence = dfs.get("team_defence")
 
     defence_dict = {}
 
     if matches is None or defence is None:
         return defence_dict
+    
+    if filters:
+
+        if filters.get("tournament_id") is not None:
+            matches = matches[
+                matches["season_id"].isin(
+                    seasons.loc[
+                        seasons["tournament_id"] == filters["tournament_id"],
+                        "season_id"
+                    ]
+                )
+            ]
+
+        if filters.get("season_id") is not None:
+            matches = matches[
+                matches["season_id"] == filters["season_id"]
+            ]
+
+        if filters.get("match_id") is not None:
+            matches = matches[
+                matches["match_id"] == filters["match_id"]
+            ]
+
+        if filters.get("team_id") is not None:
+            matches = matches[
+                (matches["home_team"] == filters["team_id"]) |
+                (matches["away_team"] == filters["team_id"])
+            ]
+
+
+    defence = defence[
+        defence["match_id"].isin(matches["match_id"])]
 
     team_name_map = {
         row.team_id: row.team_name
@@ -31,6 +63,7 @@ def build_defence_json(dfs):
         for _, d in match_defence.iterrows():
 
             inning = int(d.get("inning_no"))
+            
             team_id = int(d.get("team_id"))
             team_name = team_name_map.get(team_id, "Unknown")
 
@@ -50,5 +83,3 @@ def build_defence_json(dfs):
         defence_dict[str(m_id)] = match_structure
 
     return defence_dict
-
-
