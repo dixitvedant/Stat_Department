2ac584a707f1bfd2089b3164637540258dca8ea5
Advait1804 <pradhanadvait18@gmail.com>
2026-02-26 22:07:55 +0530

    Update database_connectivity.py

--- Python/database_connectivity.py
+++ Python/database_connectivity.py
@@ -1,21 +1,30 @@
 import mysql.connector
+import os
 import pandas as pd
 
-DB_HOST = "localhost"
-DB_USER = "root"
-DB_PASSWORD = "Advait@18"
-DB_NAME = "Kho_Kho_db"
+DB_HOST = os.environ.get("MYSQLHOST")
+DB_USER = os.environ.get("MYSQLUSER")
+DB_PASSWORD = os.environ.get("MYSQLPASSWORD")
+DB_NAME = os.environ.get("MYSQLDATABASE")
+Port = int(os.environ.get("MYSQLPORT", 3306))
 OUTPUT_DIR = "outputs"
 
 EXPECTED_TABLES = [
     "Team",
     "Player",
+    "Season",
+    "Tournament",
     "Match_details",
-    "Player_Match_Stat",
-    "Player_Season_Stat",
-    "Team_Stat",
+    "Match_Stats",
+    "Match_Awards",
+    "Player_match_stat",
+    "Player_season_stat",
+    "Player_Role_History",
+    "Team_stat",
     "Team_Attack",
-    "Team_Defence"
+    "Team_Defence",
+    "Player_Attack",
+    "Player_Defence"
 ]
 
 def connect_db():
@@ -23,7 +32,8 @@ def connect_db():
         host=DB_HOST,
         user=DB_USER,
         password=DB_PASSWORD,
-        database=DB_NAME
+        database=DB_NAME,
+        port=Port
     )
 def fetch_tables(conn):
     dfs = {}
